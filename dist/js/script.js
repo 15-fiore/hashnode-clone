07203da94f68677b6272200fd67c8478c672dcba
Ben Fiore <70206675+15-fiore@users.noreply.github.com>
2022-04-25 15:28:27 -0400

    Project II - 4.25.22
    Submission for all necessary website edits for Project II - Due 4.27.22
    Edits include:
    * Return to original website style, content structure; old structure in main.css and index_old.html
    * Addition of enlarging animation, sound cue to show mouse selection of buttons - accessibility feature/Bullet 1
    * Addition of popup to show COPPA compliance - Laws, Acts/Bullet 2
    
    * Content from Project I stored in main_edit.css and index_edit.html
    * Copy of original website repo stored at index.html

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,29 @@ function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+// Function to play sound on button hover
+function playHoverSound(soundObj) {
+  var hoverSound = document.getElementById(soundObj);
+
+  hoverSound.play();
+}
+
+// Function to stop sound after button hover ends
+function stopHoverSound(soundObj) {
+  var hoverSound = document.getElementById(soundObj);
+
+  hoverSound.pause();
+  hoverSound.currentTime = 0;
+}
+
+// Variables to track Popup, Overlay
+var closePopup = document.getElementById("popupclose");
+var overlay = document.getElementById("overlay");
+var popup = document.getElementById("popup");
+
+// Close Popup Event
+closePopup.onclick = function() {
+    overlay.style.display = 'none';
+    popup.style.display = 'none';
+}
